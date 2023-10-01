501d179f3cac84d2294d35e9b524599e3513ddb2
Adewunmi Emmanuel Olajide <olajide@Adewunmis-MacBook-Pro.local>
2023-10-01 13:01:49 +0100

    Update

---
+++ class_work1.py
@@ -0,0 +1,50 @@
+name = "Eric"
+message = f"Hello {name.upper()} would you like to learn some Python today?"
+print(message)
+
+user = "adewunmi olajide"
+print(user.lower())
+print(user.upper())
+print(user.title())
+
+quote = '"A person who never made a mistake never tried anything"'
+print(f"Albert Einstein once said, {quote}")
+
+famous_person = "Albert Einstein once said,"
+message = f"A person who never made a mistake never tried anything"
+print(famous_person, message)
+
+who = " Eric Mattens "
+print(f"\n\t{who}")
+print("Cars:\n\tToyota\tBenz\tLexus\tLambo")
+
+print(who.rstrip())
+print(who.lstrip())
+print(who.strip())
+
+print(5+3)
+print(2*4)
+print(16/2)
+print(10-2)
+
+#This is a single comment
+
+"""THIS IS A MULTILINE COMMENT Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
