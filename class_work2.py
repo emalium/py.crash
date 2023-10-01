092656f506472e9bfe302f03d64c4e68f5722e7b
Adewunmi Emmanuel Olajide <olajide@Adewunmis-MacBook-Pro.local>
2023-10-01 11:40:07 +0100

    First commit

---
+++ class_work2.py
@@ -0,0 +1,12 @@
+name = "austin kleon"
+name_1 = "david goggins"
+name_2 = "jim kwik"
+print (f"Hello {name.title()}, {name_1.upper()} and {name_2.lower()} would you like to learn some Python today?")
+
+quote = 'Albert Einstein once said, "A person who never made amistake never tried anything new"'
+print(quote)
+
+worker = "alex "
+worker_1 = " khalid"
+worker_2 = " tunde "
+print(worker.rstrip(),worker_1.lstrip(),worker_2.strip())
