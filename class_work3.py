a76d3d5cdb0c8d53fc408931e6d49e805860e9b7
Adewunmi Emmanuel Olajide <olajide@Adewunmis-MacBook-Pro.local>
2023-10-02 18:09:06 +0100

    New File

---
+++ class_work3.py
@@ -0,0 +1,36 @@
+party = ["adewunmi","emmanuel","olajide"]
+print(party[0], "are hereby invited to my party")
+print(party[1], "are hereby invited to my party")
+print(party[2], "are hereby invited to my party")
+
+unavailable = party.pop(0)
+print(f"{unavailable.title()}, won't be around" )
+party[1] = "imole"
+print(party)
+
+party = ["adewunmi","emmanuel","olajide"]
+print("Found a bigger event centre,", party)
+party.insert(0,"samuel")
+party.insert(1,"esther")
+party.insert(2,"elijah")
+print(party)
+
+print("Sorry, only two people can be invited")
+out = party.pop(-1)
+print(f"Sorry you are no longer inivited {out.title()}")
+out = party.pop(-1)
+print(f"Sorry you are no longer invited {out.title()}")
+out = party.pop(-1)
+print(f"Sorry you are no longer inivited {out.title()}")
+out = party.pop(-1)
+print(f"Sorry you are no longer inivited {out.title()}")
+
+print(party[-1], "you are still invited ma")
+print(party[-2], "you are still invited sir")
+
+del party[-1]
+print (party)
+del party [-1]
+print(party)
+
+
