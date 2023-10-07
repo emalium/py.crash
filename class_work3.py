f06dd579114c233f80130ab0599fe18391fc8656
Adewunmi Emmanuel Olajide <olajide@Adewunmis-MacBook-Pro.local>
2023-10-07 22:49:07 +0100

    you know i love this

--- class_work3.py
+++ class_work3.py
@@ -76,3 +76,38 @@ for value in range(1,11):
 print(squares) 
 #There is also another method in solving this quetion
 
+#More on List
+for value in range(1,21):
+    print(value)
+
+"""million = list(range(1,1000000))
+print(million)"""
+
+a_million = list(range(1,1_000_000))
+print(max(a_million))
+print(min(a_million))
+print(sum(a_million))
+
+"""for value in range (1,20,2):
+    print(value)"""
+
+odd_number = range(1,20,2)
+for odd in odd_number:
+ print(odd)
+
+'''for value in range (3,30,3):
+   print(value)'''
+
+threes = list(range(3,30,3))
+for three in threes:
+ print(f"\t{three}") 
+ 
+cubes = []
+for value in range(1,11):
+   cubes.append(value**3)
+print(cubes)   
+
+first_10 = list(range(1,10,3))
+print(first_10)
+    
+
