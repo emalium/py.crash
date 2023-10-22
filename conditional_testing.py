d3824410445e21e2584fac86cbd5b2274e77b05f
Adewunmi Emmanuel Olajide <olajide@Adewunmis-MacBook-Pro.local>
2023-10-22 23:05:20 +0100

    if statements

---
+++ conditional_testing.py
@@ -0,0 +1,10 @@
+internet_provider = ["mtn","glo","airtel","9mobile"]
+for internet in internet_provider:
+ if internet == "starlink":
+   print("Not part of the Nigeria Internet Provider")
+ else :
+   print("This is a Nigeria Internet Provider")
+
+crew = "adewunmi"  
+print("Is crew == 'adewunmi")
+print(crew == "adewunmi")
