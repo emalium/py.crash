12c8619967658f4644c98f17f00c8d7691bcfc65
Adewunmi Emmanuel Olajide <olajide@Adewunmis-MacBook-Pro.local>
2023-11-01 23:27:46 +0100

    learnning if statement

---
+++ if.py
@@ -0,0 +1,49 @@
+alien_color = "green"
+if alien_color == "green":
+ print("You just earned 5 points")
+ 
+alien_color = "red"
+if alien_color == "yellow":
+   print("Toor!")
+   
+alien_color = "green"   
+if alien_color == "blue":
+   print("You just earned 5 points")
+else:
+   print("You just earned 10 points")  
+
+alien_color = "red"   
+if alien_color == "green":
+   print("You just earned 5 points")
+elif alien_color == "yellow":
+   print("You just earned 10 points")       
+else :
+   print("You just earned 15 points")   
+
+
+age = 17
+if age < 2:
+   print("a baby")
+elif age < 4:
+   print("a toddler")   
+elif age < 13:
+   print("a kid")   
+elif age < 20:
+   print("a teen")   
+elif age < 65:
+   print("an adult")   
+else:
+   print("an elder")  
+   
+fav_fruits = ["watermelon","coconut","cucumber",]    
+if "watermelon" in fav_fruits:
+   print("You really like watermelon!")
+if "dates" in fav_fruits:
+   print("You really like dates!")   
+if "cucumber" in fav_fruits:
+   print("You really like cucumber!")
+if "pawpaw" in fav_fruits:
+   print("You really like pawpaw!")
+if "coconut" in fav_fruits:
+   print("You really like coconut!")   
+   
